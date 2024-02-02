8c7ec076183466de5320fd5073a00bde80d38ef5
Femi Sharomi <Mrfemi1998@gmail.com>
2024-02-02 19:55:56 +0000

    Update_header.sh
    Update to the header file to check the entire repo for the files that will need a header.

--- .bash_scripts/create_all_files_for_auto_header_generation.sh
+++ .bash_scripts/create_all_files_for_auto_header_generation.sh
@@ -56,11 +56,19 @@ chmod +x generate_header.py
 cat <<EOF > update_header.sh
 #!/bin/bash
 
-for file in "\$@"; do
-    if [[ \$file == *.c || \$file == *.cpp || \$file == *.h || \$file == *.hpp ]]; then
-        ./generate_header.py "\$file"
+# Get the absolute path to the directory containing this script
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+# Get the list of all files in the repository
+all_files=$(git ls-files)
+
+for file in $all_files; do
+    if [[ $file == *.c || $file == *.cpp || $file == *.h || $file == *.hpp ]]; then
+        # Run the generate_header.py script using an absolute path
+        "$script_dir/generate_header.py" "$file"
     fi
 done
+
 EOF
 
 # Make update_header.sh executable
--- .bash_scripts/update_header.sh
+++ .bash_scripts/update_header.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for file in "$@"; do
+# Get the absolute path to the directory containing this script
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+# Get the list of all files in the repository
+all_files=$(git ls-files)
+
+for file in $all_files; do
     if [[ $file == *.c || $file == *.cpp || $file == *.h || $file == *.hpp ]]; then
-        ./generate_header.py "$file"
+        # Run the generate_header.py script using an absolute path
+        "$script_dir/generate_header.py" "$file"
     fi
 done
