9f44b957d13f32819a0a7fd2d28c4d49d5d0b39e
Femi Sharomi <Mrfemi1998@gmail.com>
2024-02-03 04:33:47 +0000

    Test 20

--- QtQuick_Application_Tutorial/QtQuickAppTute/main.cpp
+++ QtQuick_Application_Tutorial/QtQuickAppTute/main.cpp
@@ -11,7 +11,15 @@
  * Author: Femi Sharomi
  * Date Created: 2024-02-03
  * Repository: develop
- * Description: This file is used to connect to the Qt GUI Functions
+ * Description:
+ **********************************
+ */
+/*
+ **********************************
+ * Author: Femi Sharomi
+ * Date Created: 2024-02-03
+ * Repository: develop
+ * Description: This file is used to connect to the Qt GUI Functions.
  **********************************
  */
 
