a5c1358a9bccb50eda57a71b787022b3c5337fce
Femi Sharomi <Mrfemi1998@gmail.com>
2024-02-03 04:30:31 +0000

    Test 18 - Adding a check to ensure it skips adding the header if one already exists. This will avoid clearing the description if entered or changing the dat the file was created....hopefully

--- QtQuick_Application_Tutorial/QtQuickAppTute/main.cpp
+++ QtQuick_Application_Tutorial/QtQuickAppTute/main.cpp
@@ -8,9 +8,9 @@
  */
 /*
  **********************************
- * Author: $author_name
- * Date Created: $current_date
- * Repository: $branch_repo
+ * Author: Femi Sharomi
+ * Date Created: 2024-02-03
+ * Repository: develop
  * Description: This file is used to connect to the Qt GUI Functions.
  **********************************
  */
