9404d7b4d40bc91cd46ca24b6ce05c4193febabd
Femi Sharomi <Mrfemi1998@gmail.com>
2024-03-11 23:18:45 +0000

    Added a release build

---
+++ build-notepad-Qt_6_6_0_for_macOS-Release/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/femi/Documents/GitHub/notepad/build-notepad-Qt_6_6_0_for_macOS-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/femi/Applications/Qt/6.6.0/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/femi/Applications/Qt/6.6.0/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
